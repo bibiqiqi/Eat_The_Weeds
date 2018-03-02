9879db53455d5a2cec506c392d1019d4870e4087
Bibi <bibi.samreny@protonmail.com>
2018-03-01 23:00:33 -0800

    app working only with browse by image and search by 1 plant at a time. still formatting plant summary page

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const iNAT_SEARCH_URL = 'https://api.inaturalist.org/v1/observations';
 const EDIBLES_JSON = 'http://localhost:8080/edibles.json';
-let USER_INFO = {};
 let LOCAL_JSON = {};
 let INAT_QUERY = [];
 let MAP;
 let GEOCODER;
 let MARKER;
+let USER_INFO = {};
+let BOUNDS;
 
 function renderImageGrid(item, index) {
 //generates html markup for the image grid that users can browse through.
 //will repeat the code for one image by the number of images that exist in the json files
-let imageHtml = `
-        <div class="img-grid js-img" id="index" style="background-image: url(${item.image})"></div>
-  `
+  let imageHtml = `
+    <figure id="js-figure-${index}">
+      <div class="img-grid js-img" id="${index}" style="background-image: url(${item.image})"></div>
+      <div class="img-grid-hover hidden" id="js-img-grid-hover-${index}">
+        <br><br><br><br>
+        <h1>${LOCAL_JSON.plants[index].commonName}</h1>
+        <h2>(${LOCAL_JSON.plants[index].scientificName})</h2>
+      </div>
+    </figure>
+    `
+  console.log('renderImageGrid ran');
   return (imageHtml);
 }
 
@@ -20,89 +29,102 @@ function displayImageGrid() {
   let imageGrid = LOCAL_JSON.plants.map((item, index) => renderImageGrid(item, index));
     $('#js-img-contain').html(imageGrid);
   console.log('displayImageGrid ran');
-
 }
 
 //makes JSON request and stores the data in a local file
 function makeLocalJson() {
   $.getJSON(EDIBLES_JSON, function(data) {
     LOCAL_JSON = data;
+    console.log(LOCAL_JSON);
   });
 }
 
 //function makeInatQuery() {
-//  let data = LOCAL_JSON;
-//  data.plants.forEach(function(item) {
+//  let array = LOCAL_JSON.plants;
+//  console.log(array);
+//  array.forEach(function(item) {
 //    INAT_QUERY.push(item.scientificName);
 //  })
+//  console.log('makeInatQuery ran');
 //}
 
-function getEdibleInfo(item, marker) {
-  if (item.scientificName == marker) {
-    return (`${item.edibility} <br> <a href="${item.recipes.recipeName_url}">${item.recipes.recipeName}</a>`)
-  }
+function renderPlantSummary() {
+  let item =  LOCAL_JSON.plants[USER_INFO.choice];
+  console.log(item);
+  let plantSumHtml = `
+    <h1>${item.commonName}<h1>
+    <h2>${item.scientificName}<h2>
+    <img src="${item.image}" alt="${item.scientificName}">
+    <ul>
+      <li>${item.edibleParts}</li>
+      <li><a href="${item.recipes[0].recipeName_url}">${item.recipes[0].recipeName}</a></li>
+    <ul>
+    `
+  debugger;
+  console.log('renderPlantSummary ran');
+  return (plantSumHtml);
 }
 
 //gets data from iNAT API
-//initializes a map, using Google maps API, thats centered at the geo coordinates of the user
 //sets the markers on the map
 function iNatDataToMarkers() {
+  let taxonName = LOCAL_JSON.plants[USER_INFO.choice].scientificName;
+  console.log (taxonName);
+  let lat = USER_INFO.geo.lat;
+  let lng = USER_INFO.geo.lng;
+  console.log(lat, lng);
   const query = {
-    taxon_name: "Guepinia helvelloides",
-    //order: "desc",
-    //order_by: "created_at"
-
-    //taxon_name: INAT_QUERY,
-    lat: USER_INFO.latLng.lat,
-    lng: USER_INFO.latLng.lng,
-    radius: 1,
-    quality_grade: "research",
+    taxon_name: taxonName,
+    lat: lat,
+    lng: lng,
+    radius: "100",
     mappable: true,
-    geo: true
+    geo: true,
   };
   $.getJSON(iNAT_SEARCH_URL, query, function (data) {
-  //$.getJSON("https://api.inaturalist.org/v1/observations?taxon_name=%22Guepinia%20helvelloides%22&order=desc&order_by=created_at", function (data) {
-    //callback function for json request
     let results = data.results;
-    console.log(results);
-    //results.forEach(function(result) {
+    console.log (results);
+    BOUNDS = new google.maps.LatLngBounds();
+    let markerGeo = {};
+    results.forEach(function(result) {
+      markerGeo.lat = parseFloat(result.geojson.coordinates[1]);
+      markerGeo.lng = parseFloat(result.geojson.coordinates[0]);
     //for each result item, create a marker at corresponding location
-    //  MARKER = new google.maps.Marker({
-    //    position: results.location,
-    //    map: MAP
-    //  });
-    //and a corresponding info window
-    //resultsArray.taxon.name
-    //  let infoWindow = new google.maps.InfoWindow({
-    //    content: `
-    //      <h1>Scientific Name: ${results.taxon.name}</h1>
-    //      <h2>Common Name: ${results.preferred_common_name}</h2>
-    //      <p>${iterateThroughJson(getEdibleInfo(), results.taxon.name)}</p>
-    //      `
-    //  });
+      MARKER = new google.maps.Marker({
+        position: markerGeo,
+        map: MAP
+      });
+      let loc = new google.maps.LatLng(markerGeo.lat, markerGeo.lng);
+      BOUNDS.extend(loc);
     });
-  //});
+    // auto-zoom
+    MAP.fitBounds(BOUNDS);
+    //auto-center
+    MAP.panToBounds(BOUNDS);
+  });
+  console.log('iNatDataToMarkers ran');
 }
 
-//converts zipcode to Geo Coordinates and assigns those values to the property LatLng in the global object USER_INFO
+//converts zipcode to Geo Coordinates and assigns those values to the property geo in the global object USER_INFO
 //results is coming back undefined, for some reason
 function addressToGeo() {
-  USER_INFO.latLng = {};
-  console.log (USER_INFO.userAddress);
+  USER_INFO.geo = {};
+  console.log (USER_INFO.address);
   GEOCODER = new google.maps.Geocoder();
-  GEOCODER.geocode( { 'address': USER_INFO.userAddress }, function(results, status) {
+  GEOCODER.geocode( { 'address': USER_INFO.address }, function(results, status) {
     if (status == 'OK') {
-      USER_INFO.latLng.lat = results[0].geometry.location.lat();
-      USER_INFO.latLng.lng = results[0].geometry.location.lng();
-      MAP.setCenter(USER_INFO.latLng);
-      //console.log(USER_INFO.latLng);
+      USER_INFO.geo.lat = results[0].geometry.location.lat();
+      USER_INFO.geo.lng = results[0].geometry.location.lng();
+      MAP.setCenter(USER_INFO.geo);
+      //console.log(USER_INFO.geo);
       ////debugger;
     } else {
-     alert("Geocode was not successful for the following reason: " + status);
+    // alert("Geocode was not successful for the following reason: " + status);
     }
-    //debugger;
-    //alert('Latitude: ' + USER_INFO.latLng.lat + ' Longitude: ' + USER_INFO.latLng.lng);
+    //alert('Latitude: ' + USER_INFO.geo.lat + ' Longitude: ' + USER_INFO.geo.lng);
+    iNatDataToMarkers();
   });
+  console.log('addressToGeo ran');
 }
 
 function initMap() {
@@ -110,63 +132,76 @@ function initMap() {
      center: {lat: 0, lng: 0},
      zoom: 1
    });
+   console.log('initMap ran');
 }
 
 //event listeners
 
 //when user hovers over an image, div with plant name appears above image
-function hoverGetsInfo() {
+function hoverShowsName() {
   $('.js-img').hover(
     function() {
-      console.log(this);
-      debugger;
-      let whichWeed = this.attr('id');
-      console.log(whichWeed);
-      console.log(this.parent);
-      debugger;
-      $(this.parent.append(`
-        <div class="img-grid-hover">
-          <h1>${LOCAL_JSON.plants[whichWeed].scientificName}</h1>
-          <h2>${LOCAL_JSON.plants[whichWeed].commonName}</h2>
-        </div>
-        `));
-    }, function() {
-      $(this).remove('.img-grid-hover');
+      let index = $(this).attr('id');
+      console.log(index);
+      $(`#js-img-grid-hover-${index}`).removeClass('hidden');
+ }, function() {
+      $(`#js-img-grid-hover-${index}`).addClass('hidden');
     }
   );
+  console.log('hoverShowsName ran');
+}
+
+function searchAgain() {
+  $('#js-search-again').on("click", function(event) {
+    //Clear out the user info
+    Object.keys(USER_INFO).forEach(function (prop) {
+      delete USER_INFO[prop];
+      });
+    $('#js-results').addClass('hidden');
+    $('#js-img-browse').removeClass('hidden');
+    displayImageGrid();
+    //hoverShowsName();
+    resultsPage();
+    console.log('searchAgain ran');
+  });
 }
 
 function doTheMainPage() {
   $('#js-enter').on("click", function(event) {
+    //makeInatQuery();
     $('#js-cover-page').addClass('hidden');
     $('#js-main-page').removeClass('hidden');
     $('#js-img-browse').removeClass('hidden');
     displayImageGrid();
-    console.log('doTheMainPage ran')
-    //hoverGetsInfo();
+    hoverShowsName();
+    resultsPage();
+    console.log('doTheMainPage ran');
   });
 }
 
+function resultsPage() {
+  $('.js-img').on("click", function(event) {
+    USER_INFO.choice = $(this).attr('id');
+    console.log(USER_INFO.choice);
+    $('#js-main-page').addClass('hidden');
+    $('#js-img-browse').addClass('hidden');
+    $('#js-plant-info').html(renderPlantSummary());
+    $('#js-results').removeClass('hidden');
+    doTheMap();
+    searchAgain();
+    console.log('resultsPage ran');
+  });
+}
+
+//call doTheMap with iNatDataToMarkers() as the callback function
 function doTheMap() {
   $('#js-location-submit').on("click", function(event) {
     event.preventDefault();
-    //get the value of the user's zipcode
-    USER_INFO.userAddress = $('#address').val();
-    //console.log(USER_INFO.userAddress);
-    //debugger;
-    //create an array of scientific names for user's chosen filter, and store as a property of global object USER_INFO,
-    // that will be used as a search query for the iNat API
-    makeInatQuery();
-    //console.log(INAT_QUERY);
-    //debugger;
-    //convert the users zipCode to geo coordinates and store as a property of global object USER_INFO
+    //get the value of the user's address
+    USER_INFO.address = $('#address').val();
     addressToGeo();
-    //queries iNat api with the array of scientific names and the user's location, creates a map, and sets markers According
-    //to the locations of the iNat observations that match the query
-    //calls iterateThroughJson() and getEdibleInfo()
-    iNatDataToMarkers();
+    console.log('doTheMap ran');
   });
-
  }
 //makes JSON request and stores data as local variable
 makeLocalJson();
