5b5a352c61a67ac711706524949ec61653499d4a
Bibi <bibi.samreny@protonmail.com>
2018-03-11 23:56:08 -0700

    a11y done, and more cleaning up

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const iNAT_SEARCH_URL = 'https://api.inaturalist.org/v1/observations';
-const EDIBLES_JSON = 'http://localhost:8080/edibles.json';
+const EDIBLES_JSON = 'edibles.json'
 let LOCAL_JSON = {};
 let INAT_QUERY = [];
 let MAP;
@@ -8,20 +8,18 @@ let MARKER;
 let USER_INFO = {};
 let BOUNDS;
 
-//intitializes results map
+// intitializes results map
 function initMap() {
    MAP = new google.maps.Map(document.getElementById('map'), {
      center: {lat: 0, lng: 0},
      zoom: 1
    });
-   console.log('initMap ran');
 }
 
-//makes JSON request and stores the data in a local file
+// makes JSON request and stores the data in a local file
 function makeLocalJson(callback) {
   $.getJSON(EDIBLES_JSON, function(data) {
     LOCAL_JSON = data;
-    console.log(LOCAL_JSON);
     callback();
   });
 }
@@ -37,14 +35,12 @@ function renderImageGrid(item, index) {
       </div>
     </figure>
     `
-  console.log('renderImageGrid ran');
   return (imageHtml);
 }
 
 function displayImageGrid() {
   let imageGrid = LOCAL_JSON.plants.map((item, index) => renderImageGrid(item, index));
     $('#grid').html(imageGrid);
-  console.log('displayImageGrid ran');
 }
 
 //function makeInatQuery() {
@@ -96,31 +92,36 @@ function renderPlantSummary() {
 //in USER_INFO
 function addressToGeo() {
   USER_INFO.geo = {};
-  console.log (USER_INFO.address);
   GEOCODER = new google.maps.Geocoder();
   GEOCODER.geocode( { 'address': USER_INFO.address }, function(results, status) {
     if (status == 'OK') {
       USER_INFO.geo.lat = results[0].geometry.location.lat();
       USER_INFO.geo.lng = results[0].geometry.location.lng();
       MAP.setCenter(USER_INFO.geo);
-      //console.log(USER_INFO.geo);
-      ////debugger;
     } else {
-    // alert("Geocode was not successful for the following reason: " + status);
     }
-    //alert('Latitude: ' + USER_INFO.geo.lat + ' Longitude: ' + USER_INFO.geo.lng);
     iNatDataToMarkers();
   });
-  console.log('addressToGeo ran');
+}
+
+function noResults() {
+  $('#plant-info').addClass('hidden');
+  $('form').addClass('hidden');
+  $('#map').addClass('hidden');
+  $('#no-results').removeClass('hidden');
+  noResultsSearch();
+}
+
+function revealMap() {
+  $('#map').removeClass('hidden');
+  $('.search-again').removeClass('hidden');
 }
 
 // makes request to iNAT API, sets the markers on the map, and zooms into map
 function iNatDataToMarkers() {
   let taxonName = LOCAL_JSON.plants[USER_INFO.choice].scientificName;
-  console.log (taxonName);
   let lat = USER_INFO.geo.lat;
   let lng = USER_INFO.geo.lng;
-  console.log(lat, lng);
   const query = {
     taxon_name: taxonName,
     lat: lat,
@@ -131,13 +132,8 @@ function iNatDataToMarkers() {
   };
   $.getJSON(iNAT_SEARCH_URL, query, function (data) {
     let results = data.results;
-    console.log (results);
     if (results.length == 0) {
-      $('#plant-info').addClass('hidden');
-      $('form').addClass('hidden');
-      $('#map').addClass('hidden');
-      $('#no-results').removeClass('hidden');
-      noResultsSearch();
+    noResults();
     }
     else {
       BOUNDS = new google.maps.LatLngBounds();
@@ -145,7 +141,6 @@ function iNatDataToMarkers() {
       results.forEach(function(result) {
         markerGeo.lat = parseFloat(result.geojson.coordinates[1]);
         markerGeo.lng = parseFloat(result.geojson.coordinates[0]);
-      //for each result item, create a marker at corresponding location
         MARKER = new google.maps.Marker({
           position: markerGeo,
           map: MAP
@@ -153,15 +148,11 @@ function iNatDataToMarkers() {
         let loc = new google.maps.LatLng(markerGeo.lat, markerGeo.lng);
         BOUNDS.extend(loc);
       });
-      // auto-zoom
       MAP.fitBounds(BOUNDS);
-      //auto-center
       MAP.panToBounds(BOUNDS);
-      $('#map').removeClass('hidden');
-      $('.search-again').removeClass('hidden');
+      revealMap();
     }
   });
-  console.log('iNatDataToMarkers ran');
 }
 
 // event listeners
@@ -175,25 +166,16 @@ function doTheMainPage() {
     displayImageGrid();
     hoverShowsName();
     resultsPage();
-    console.log('doTheMainPage ran');
   });
 }
 
 // listens for user to hover over image in img grid to display plant name
 function hoverShowsName() {
   $('.figures').hover(function(event) {
-    //let index = $(this).attr('id');
-    //console.log(index);
-    //debugger;
-    //let figureId = $(event.currentTarget).attr('id');
     $(this).find('.img-hover').removeClass('hidden');
-    //$(`#js-img-grid-hover-${index}`).removeClass('hidden');
   }, function() {
-    //let index = $(this).attr('id');
     $(this).find('.img-hover').addClass('hidden');
-    //$(`#js-img-grid-hover-${index}`).addClass('hidden');
   });
-  console.log('hoverShowsName ran');
 }
 
 // listens for user to select one of the plant images to display results page
@@ -208,7 +190,6 @@ function resultsPage() {
     $('#results-page').removeClass('hidden');
     doTheMap();
     searchAgain();
-    console.log('resultsPage ran');
   });
 }
 
@@ -220,7 +201,6 @@ function doTheMap() {
     $('form').addClass('hidden');
     USER_INFO.address = $('#address').val();
     addressToGeo();
-    console.log('doTheMap ran');
   });
  }
 
@@ -252,7 +232,6 @@ function searchAgain() {
     $('#map').addClass('hidden');
     $('form').removeClass('hidden');
     resultsPage();
-    console.log('searchAgain ran');
   });
 }
 //makes JSON request and stores data as local variable
